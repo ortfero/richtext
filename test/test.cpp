9f6124669ff3261979deb896786e95369c261ca1
Andrei Ilin <ortfero@gmail.com>
2020-07-10 15:21:52 +0300

    Test case

--- test/test.cpp
+++ test/test.cpp
@@ -2,7 +2,33 @@
 #include <doctest/doctest.h>
 
 #include <richtext/richtext.hpp>
+#include <richtext/formatters/markdown.hpp>
 
 
 TEST_CASE("richtext") {
+
+  using namespace richtext;
+  auto md = formatters::markdown("test.md");
+  auto doc = document{ "Document Header" }
+    .add(paragraph{ "This is paragraph" })
+    .add(paragraph{}
+      .add("This ")
+      .add(tag::strong, "is")
+      .add(" ")
+      .add(tag::emphasis, "formatted")
+      .add(" ")
+      .add(tag::strong_emphasis, "text"))
+    .add(section{ "Section Header" }
+      .add(unordered_list{ "Unordered items:" }
+        .add(paragraph{ "Item 1" })
+        .add(paragraph{ "Item 2" })
+        .add(paragraph{ "Item 3" })))
+    .add(subsection{ "Subsection Header" }
+      .add(ordered_list{"Ordered items:"}
+        .add(paragraph{ "Item 1" })
+        .add(paragraph{ "Item 2" }))
+      .add(table{ {"Column A", "Column B"} })
+        /*.add()*/);
+  std::error_code ec;
+  md.render(doc, ec);
 }
